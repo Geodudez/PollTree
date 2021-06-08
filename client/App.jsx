3895fcfaeafa16439ca869c77c9141ca823e22cc
Abigail Gjurich <amgjurich@gmail.com>
2021-06-08 16:56:14 -0400

    got a d3 pie graph rendering with fake data

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import Profile from './components/Profile.jsx';
-import Pie from './components/Pie.jsx';
-import Bar from './components/Bar.jsx';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import * as d3 from 'd3';
+import Pie from './components/Pie';
 
-export default function App() {
+function App() {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value:
+        value === null || value === undefined ? Math.random() * 100 : value,
+    }));
+
+  const [data, setData] = useState(generateData(0));
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  useEffect(() => {
+    setData(generateData());
+  }, [!data]);
   return (
-    <div>
-      <Profile />
-      <Pie />
-      <Bar />
+    <div className='App'>
+      <div>
+        <span className='label'>Hooks</span>
+        <Pie
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
     </div>
   );
 }
 
+//yo wassup dawgs
 
-//yo wassup dawgs 
+export default App;
