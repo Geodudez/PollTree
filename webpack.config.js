da825039d0192195bad0ac0617f8a46f528a29d6
Seymour-creates <romelogilbert@yahoo.com>
2021-06-08 09:03:48 -0400

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, 'public/build')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+      main: './src/index.js',
+    },
+    output: {
+      filename: 'bundle.js',
+      path: BUILD_DIR,
+    },
+    target: 'node',
+    resolve: {
+      fallback: {
+        fs: false,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+            },
+          ],
+        },
+      ],
+    },
+  };
