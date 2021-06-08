1e28594e957e3865b516537b1bf9022952e6e207
Abigail Gjurich <amgjurich@gmail.com>
2021-06-08 12:44:00 -0400

    got the webpack dev server going

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './components/Profile.jsx';
+import Pie from './components/Pie.jsx';
+import Bar from './components/Bar.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Pie />
+      <Bar />
+    </div>
+  );
+}
