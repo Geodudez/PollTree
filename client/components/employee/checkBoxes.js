2994e48fcc25291c45a44ea2cf21d983c72d3463
Seymour-creates <romelogilbert@yahoo.com>
2021-06-08 23:16:03 -0400

    architecture updates

---
+++ client/components/employee/checkBoxes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function CheckBoxes() {
+
+    return (
+        <p>I'm going to be check boxes when i grow up</p>
+    )
+}
--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
+import style from '../public/build/styles.css'
+
 
 render(<App />, document.getElementById('root'));
