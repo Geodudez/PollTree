822f80b3bb4545c0402568adaf3fb472616579db
Seymour-creates <romelogilbert@yahoo.com>
2021-06-09 12:10:15 -0400

    rotating poll questions

--- client/components/employee/pollQuestions.js
+++ client/components/employee/pollQuestions.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import CheckBoxes from './checkBoxes'
+import {useState} from 'react'
+import NextButton from './nextButton'
 
 export default function PollQuestion() {
+    let [currentQuestion,setCurrentQuestion ] = useState(0)
+    const questions = [
+        'I am pleased to work with the current version of this technology.'
+        ,'I experience few limitations with this technology.'
+        ,'I find this technology integrates into exist codebases with relative ease.'
+        ,'I feel there are necessary improvements to be made for the next version of this technology.'
+        ,'I often find myself researching solutions to issues that occurred in this technology.'
+        ,'I would recommend this technology for others to use.'
+    ];
+    console.log(questions.length)
+    const displayQuestion = []
+    questions.forEach(question => {
+        displayQuestion.push(<CheckBoxes question={question}/>)
+    } )
+    console.log(displayQuestion)
+
+
     return (
-        <p> I'll be some poll questions</p>
+        <div>
+        {displayQuestion[currentQuestion]}
+        <NextButton setQuestion={setCurrentQuestion} currentQuestion={currentQuestion}/>
+        </div>
     )
 }
